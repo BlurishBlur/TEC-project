95d0b961aafc6e6eb100798e8b34d24077794495
Niels Heltner <niels.heltner@hotmail.com>
2017-10-09 23:56:23 +0200

    sat routing op

--- Server/server.js
+++ Server/server.js
@@ -1,23 +1,10 @@
 #!/usr/bin/env node
 
-// http://blog.modulus.io/build-your-first-http-server-in-nodejs
-
-var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3000;
-
-app.listen(port);
-
-console.log('todo list RESTful API server started on: ' + port);
-
-
-
-
-
-
-
-var http   = require('http');
+var http = require('http');
 const PORT = 9998;
+
+var users = [];
+users[users.length] = {name: "Niels"};
 
 send_header = function (response) {
     response.writeHead(200, {'Content-Type': 'application/json',
@@ -25,45 +12,22 @@ send_header = function (response) {
                              'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE'});
 }
 
-var library = [{author: "Anton", title: "Livet om Nils", ISBN: "1234"},
-              {author: "Niels", title: "Livet om Anton", ISBN: "2345"}];
-
-search = function (search) {
-    for (var i = 0; i < library.length; i++) {
-        if(library[i].author == search) {
-            return library[i];
-        }
-    }
+getUsers = function(request, response) {
+    response.end(JSON.stringify(users));
 }
 
-putInArray = function(dataFromClient) {
-    var data = dataFromClient.split(",");
-    if(data.length != 3) {
-        return "Der gik noget galt";
-    } else {
-        library.push({author: data[0], title: data[1], ISBN: data[2]});
-        return "Det gik godt!";
-    }
-}
-
-deleteFromArray = function (data) {
-    for (var i = 0; i < library.length; i++) {
-        if(library[i].author == data) {
-            library.splice(i, 1);
-        }
-    }
-}
-
-// handlers
+// http handlers
 handler_get = function (request, response) {
     send_header(response);
-    response.end(JSON.stringify(library));
+    //response.status(404).end("Not found");
+    //response.end(JSON.stringify(library));
 };
 handler_put = function (request, response) {
     request.on('data', function(data) {
         console.log(''+data);
         send_header(response);       
-        response.end(JSON.stringify(putInArray(''+data)));
+        //response.end(JSON.stringify(putInArray(''+data)));
+        response.end(JSON.stringify('ALTING ER GODT'));
     });
 };
 handler_post = function (request, response) {
@@ -85,17 +49,32 @@ handler_options = function (request, response) {
     response.end(null);
 };
 
-dispatch = {
-    'GET':     handler_get,
-    'PUT':     handler_put,
-    'POST':    handler_post,
-    'DELETE':  handler_delete,
-    'OPTIONS': handler_options,
+routes = {
+    'GET/users':     getUsers,
+    'PUT/users':     handler_put,
+    'POST':          handler_post,
+    'DELETE':        handler_delete,
+    'OPTIONS':       handler_options,
 };
 
+handleRequest = function(request, response) {
+    send_header(response);
+    var routedRequest = request['method'] + request.url;
+    if(typeof(routes[routedRequest]) === 'function') {
+        routes[routedRequest](request, response);
+    }
+    else {
+        response.end("Not found");
+    }
+}
+
 var server = http.createServer(function (request, response){
-    console.log(request['method']+' '+request.url);
-    dispatch[request['method']](request, response);
+    console.log("Received request for " + request['method'] + request.url);
+    handleRequest(request, response);
+
+
+    //console.log(request['method']+' '+request.url);
+    //dispatch[request['method']](request, response);
 });
 server.listen(PORT, function(){
     console.log("Listening to http://localhost:%s", PORT);
