705f5ca16188f9dbfb1e33a3e4ed0912cef34148
LascariTheMan <lascaritheman@gmail.com>
2017-10-14 02:23:00 +0200

    fjernet lidt shit

--- Client/js/createAccount.js
+++ Client/js/createAccount.js
@@ -4,11 +4,13 @@ var createAccount = angular.module('createAccount', []);
 createAccount.controller('checkInput', function ($scope) {
     "use strict";
     
-    $scope.createUser = function (password, repeat) {
-        console.log(password);
-        $scope.returnMessage = "You clicked!";
-        if (password !== repeat || password.length < 6) {
-            $scope.returnMessage = "Please check that password matches, and it's more than 6 characters";
+    $scope.createUser = function (password, repeat, user) {
+        if (user === undefined || user === "") {
+            $scope.returnMessage = "Please write a username.";
+        } else if (password.length < 6) {
+            $scope.returnMessage = "Please check that password is more than 6 characters.";
+        } else if (user === password) {
+            $scope.returnMessage = "Username and password cannot be the same.";
         } else {
             $scope.returnMessage = "User Created!";
         }
