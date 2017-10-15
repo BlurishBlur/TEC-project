a0fabd6cbeb4608226dd85ef2bc324cc7a81eef1
Niclas Mølby <niclas@molby.net>
2017-10-15 17:48:40 +0200

    Ændret lidt i validering og styling

--- Client/js/createAccount.js
+++ Client/js/createAccount.js
@@ -42,8 +42,6 @@ http.controller('httpCtrl', function ($scope) {
         });
     };
     
-    $j("#create").click($scope.do_put);
-    
     function colorUser(value) {
         $scope.styleUser = function () {
             return { "border": value };
@@ -55,28 +53,62 @@ http.controller('httpCtrl', function ($scope) {
             return { "border": value };
         };
     }
+
+    function reset() {
+        $scope.usernameReturnMessage = "";
+        $scope.styleUser = function () {
+            return { "border": "3px solid #9EA9AB" };
+        };
+
+        $scope.passwordReturnMessage = "";
+        $scope.repeatPasswordReturnMessage = "";
+        $scope.stylePassword = function () {
+            return { "border": "3px solid #9EA9AB" };
+        };
+    }
     
     $scope.createUser = function () {
+        reset();
+        var errors = 0;
+        // Check username
         if ($scope.user === undefined || $scope.user === "") {
-            $scope.returnMessage = "Please write a username.";
+            $scope.usernameReturnMessage = "Please write a username.";
             colorUser("3px solid #840200");
-            colorPassword("3px solid #9EA9AB");
-        } else if ($scope.password.length < 6) {
-            $scope.returnMessage = "Please check that password is more than 6 characters.";
+            errors++;
+        }
+        // Check password
+        if ($scope.password == undefined || $scope.password.length < 6) {
+            $scope.passwordReturnMessage = "Please check that password is more than 6 characters.";
             colorPassword("3px solid #840200");
-            colorUser("3px solid #9EA9ABs");
-        } else if ($scope.user === $scope.password) {
-            $scope.returnMessage = "Username and password cannot be the same.";
-            colorUser("3px solid #840200");
+            errors++;
+        } 
+        // Check if pass and username is equal
+        if ($scope.user != undefined && $scope.password != undefined && $scope.user === $scope.password) {
+            $scope.passwordReturnMessage = "Username and password cannot be the same.";
             colorPassword("3px solid #840200");
-        } else {
+            errors++;
+        } 
+        // Check if passwords matches
+        if ($scope.password === undefined || $scope.repeat === undefined || $scope.password !== $scope.repeat) {
+            $scope.repeatPasswordReturnMessage = "Password does not match.";
+            colorPassword("3px solid #840200");
+            errors++;
+        }
+
+        // Final error check
+        if (errors === 0) {
             $scope.returnMessage = "User Created!";
-            colorUser("3px solid #9EA9ABs");
-            colorPassword("3px solid #9EA9AB");
+            colorUser("3px solid #1B5E20");
+            colorPassword("3px solid #1B5E20");
+
+            //$scope.do_put;
+        }
+        else {
+            $scope.returnMessage = "There were " + errors + " error(s)." ;
         }
     };
     
-    $scope.checkPasswordMatch = function () {
+    /*$scope.checkPasswordMatch = function () {
         if ($scope.repeat === "") {
             $scope.passwordMatchMessage = "";
         } else if ($scope.password === $scope.repeat) {
@@ -84,5 +116,5 @@ http.controller('httpCtrl', function ($scope) {
         } else {
             $scope.passwordMatchMessage = "Password does not match";
         }
-    };
+    };*/
 });
